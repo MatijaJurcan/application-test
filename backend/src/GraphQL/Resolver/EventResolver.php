adbfd576171ca62ab797baa985e9682ff4cdd681
MatijaJurcan <matija@tahoma.hr>
2024-07-22 09:03:33 +0200

    PHP tasks

--- backend/src/GraphQL/Resolver/EventResolver.php
+++ backend/src/GraphQL/Resolver/EventResolver.php
@@ -88,20 +88,53 @@ final class EventResolver implements ResolverInterface
 
     private function resolveParticipants(Event $event, ArgumentInterface $args): array
     {
-        $queryString = $args['queryString'];
-        // TODO: 
-        // If queryString is provided, filter participants by it 
-        // and return all participants that contain the query string.
-        // The search should be case-insensitive.
-        // If the query string is not provided, return all participants.
-        return $event->getParticipants();
+        $queryString = isset($args['queryString']) ? strtolower($args['queryString']) : ''; // setting queryString to lowercase in a single statement if queryString exists
+        $participants = $event->getParticipants(); 
+        if ($queryString !== '') {
+            $lowercaseParticipants = array_map('strtolower', $participants); 
+            // Precomputed lowercase participants to eliminate stripos internal strtolower calls
+            $filteredIndexes = array_keys(array_filter($lowercaseParticipants, function($participant) use ($queryString) {
+                return stripos($participant, $queryString) !== false;
+            }));
+    
+            $participants = array_intersect_key($participants, array_flip($filteredIndexes));
+        }
+
+        // Explanation:
+        // A possible solution here was also:
+//      $queryString = strtolower($queryString);
+//      $participants = array_filter($participants, function($participant) use ($queryString) {
+//          return stripos($participant, $queryString) !== false;
+//      });
+//         
+        //
+        // While this code might be simpler to read, and uses less memory - it's not computationally efficient.
+        // With the previous approach of minimizing stripos() internal strtolower() calls using a precomputed array of lowercase participants, 
+        // redundant operations have been removed since we call strtolower() just once versus calling it for every participant.
+        // Note also that array_keys() and array_intersect_key() increase minimally memory usage overhead instead of opting to not use them, but in this case it's well worth it.
+        // The benefits of this approach grow with with the size of the dataset.
+    
+        return $participants;
     }
 
     private function resolveProgram(Event $event): Collection
     {
-        // TODO:
-        // Event::program is a collection of Speech objects.
-        // Return the event's program with the speeches sorted by startTime.
-        return $event->getProgram();
+
+        $program = $event->getProgram();
+        $sortedProgram = $program->sort(function ($a, $b) {
+            return $a->getStartTime() <=> $b->getStartTime();
+        });
+
+        // Note that depending on the size of the dataset, we can optimize this further by turning the Doctrine collection to an array, 
+        // using usort function for sorting and converting back to a collection. Here is an example:
+//      $speeches = $program->toArray();
+//      usort($speeches, function ($a, $b) {
+//         return $a->getStartTime() <=> $b->getStartTime();
+//      });
+//      $sortedProgram = new ArrayCollection($speeches);
+        // The main advantage of this approach is minimizing Doctrine's object-oriented overhead and other abstractions that negatively impact performance.
+        // Worth noting is that the computation overhead in this case it's minimal since Doctrine Collection sort() method already uses usort() internally and that is why I opted to use sort() in my solution.
+        
+        return $sortedProgram;
     }
 }
